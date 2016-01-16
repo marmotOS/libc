1448a80a11a8c5cfad0708294ad76ca20f2bfe89
Bryant Moscon <bmoscon@gmail.com>
2016-01-15 19:57:48 -0500

    Add errno

---
+++ src/errno.c
@@ -0,0 +1,15 @@
+/*
+ * errno.c
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#include <errno.h>
+
+
+int errno = 0;
---
+++ src/errno.h
@@ -0,0 +1,22 @@
+/*
+ * errno.h
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __ERRNO__
+#define __ERRNO__
+
+#define EDOM 1
+#define EDOM 2
+
+
+extern int errno;
+
+
+#endif
