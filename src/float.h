6e4c7422b93a76c4ba58e01bb4becb8d01c4d099
Bryant Moscon <bmoscon@gmail.com>
2016-01-20 19:03:31 -0500

    add float.h

---
+++ src/float.h
@@ -0,0 +1,47 @@
+/*
+ * float.h
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __FLOAT__
+#define __FLOAT__
+
+#define DBL_DIG 
+#define DBL_EPSILON  
+#define DBL_MANT_DIG
+#define DBL_MAX           
+#define DBL_MAX_10_EXP       
+#define DBL_MAX_EXP
+#define DBL_MIN         
+#define DBL_MIN_10_EXP       
+#define DBL_MIN_EXP
+#define FLT_DIG              
+#define FLT_EPSILON        
+#define FLT_MANT_DIG
+#define FLT_MAX                
+#define FLT_MAX_10_EXP      
+#define FLT_MAX_EXP
+#define FLT_MIN              
+#define FLT_MIN_10_EXP      
+#define FLT_MIN_EXP
+#define FLT_RADIX             
+#define FLT_ROUNDS
+#define LDBL_DIG             
+#define LDBL_EPSILON            
+#define LDBL_MANT_DIG
+#define LDBL_MAX         
+#define LDBL_MAX_10_EXP       
+#define LDBL_MAX_EXP
+#define LDBL_MIN          
+#define LDBL_MIN_10_EXP    
+#define LDBL_MIN_EXP
+
+
+
+#endif
