7b6ff68478de12bd27daacf5a12ebb50da7908c2
Bryant Moscon <bmoscon@gmail.com>
2016-01-27 16:30:46 -0500

    fix issue #1 - support EOF as legal value

--- src/ctype.c
+++ src/ctype.c
@@ -12,7 +12,12 @@
 #include <ctype.h>
 
 
-unsigned char _ctype[256] = {
+// NOTE:
+// ctype tables are indexed by adding 1 to index
+// so that EOF indexes to zero
+
+
+unsigned char _ctype[257] = { 0,
   0,_B,_B,_B,_B,_B,_B,_B,_B,_B,_C,_C,_C,_C,_C,_B,
   _B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,_B,
   _S,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,_P,
@@ -33,7 +38,7 @@ unsigned char _ctype[256] = {
 };
 
 
-unsigned char _lower[256] = {
+unsigned char _lower[257] = { 0x00,
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
     0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
     0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
@@ -55,72 +60,72 @@ unsigned char _lower[256] = {
 
 int isalpha(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_U | _L));
+    return (_ctype[x+1] & (_U | _L));
 }
 
 
 int isalnum(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_D | _U | _L));
+    return (_ctype[x+1] & (_D | _U | _L));
 }
 
 
 int iscntrl(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_B | _C));
+    return (_ctype[x+1] & (_B | _C));
 }
 
 
 int isdigit(const int x)
 {
-    return (_ctype[(unsigned char) x] & _D);
+    return (_ctype[x+1] & _D);
 }
 
 
 int isgraph(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_D | _L | _U | _P));
+    return (_ctype[x+1] & (_D | _L | _U | _P));
 }
 
 
 int islower(const int x)
 {
-    return (_ctype[(unsigned char) x] & _L);
+    return (_ctype[x+1] & _L);
 }
 
 
 int isprint(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_D | _L | _U | _P | _S));
+    return (_ctype[x+1] & (_D | _L | _U | _P | _S));
 }
 
 
 int ispunct(const int x)
 {
-    return (_ctype[(unsigned char) x] & _P);
+    return (_ctype[x+1] & _P);
 }
 
 
 int isspace(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_S | _C));
+    return (_ctype[x+1] & (_S | _C));
 }
 
 
 int isupper(const int x)
 {
-    return (_ctype[(unsigned char) x] & _U);
+    return (_ctype[x+1] & _U);
 }
 
 
 int isxdigit(const int x)
 {
-    return (_ctype[(unsigned char) x] & (_X | _D));
+    return (_ctype[x+1] & (_X | _D));
 }
 
 
 int tolower(const int x)
 {
-    return (_lower[(unsigned char) x]);
+    return (_lower[x+1]);
 }
 
