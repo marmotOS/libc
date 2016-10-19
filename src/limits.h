1729393fcc8bc40a84000523f270837b9be518f8
Bryant Moscon <bmoscon@gmail.com>
2016-10-18 20:08:52 -0400

    initial work on limits

---
+++ src/limits.h
@@ -0,0 +1,23 @@
+/*
+ * limits.h
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+#ifndef __LIMITS__
+#define __LIMITS__
+
+#define CHAR_BITS    8
+#define CHAR_MAX     127
+#define CHAR_MIN     -128
+
+#define INT_MAX      2147483647
+#define INT_MIN      -2147483648
+#define UINT_MAX     4294967295
+
+
+#endif
