a0cd2cdc1d47efd78e1b9f7b893c64a54a39bd11
Bryant Moscon <bmoscon@gmail.com>
2016-10-22 14:06:51 -0400

    complete limits.h

--- src/limits.h
+++ src/limits.h
@@ -8,16 +8,30 @@
  * the use of this software.
  *
  */
+
 #ifndef __LIMITS__
 #define __LIMITS__
 
 #define CHAR_BITS    8
 #define CHAR_MAX     127
 #define CHAR_MIN     -128
+#define UCHAR_MAX    255
 
 #define INT_MAX      2147483647
 #define INT_MIN      -2147483648
 #define UINT_MAX     4294967295
 
+#define LONG_MAX     9223372036854775807
+#define LONG_MIN     -9223372036854775808
+#define ULONG_MAX    18446744073709551615
+
+#define MB_LEN_MAX   16
+
+#define SCHAR_MAX    127
+#define SCHAR_MIN    -128
+
+#define SHRT_MAX     32767
+#define SHRT_MIN     -32768
+#define USHRT_MAX    65535
 
 #endif
