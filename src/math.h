096c697c1872e7bbf6ccf88c22cc067ad6578df5
Bryant Moscon <bmoscon@gmail.com>
2016-10-22 14:10:04 -0400

    start of math.h

---
+++ src/math.h
@@ -0,0 +1,18 @@
+/*
+ * math.h
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __MATH__
+#define __MATH__
+
+
+
+
+#endif
