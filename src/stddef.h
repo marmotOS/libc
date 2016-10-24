61b52c18dd0ec88b460aebd388af7e36987d9ad4
Bryant Moscon <bmoscon@gmail.com>
2016-10-23 20:57:36 -0400

    add stddef.h

---
+++ src/stddef.h
@@ -0,0 +1,22 @@
+/*
+ * stddef.h
+ *
+ *
+ * Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __STDDEF__
+#define __STDDEF__
+
+
+#define size_t       unsigned long int
+#define ptrdiff_t    long int
+#define wchar_t      int
+#define NULL         (void *)0
+
+
+#endif
