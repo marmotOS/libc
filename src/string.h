ea7fcfb214c388b3152f853b1a0a6d1a50711e36
Bryant Moscon <bmoscon@gmail.com>
2017-01-18 19:33:21 -0500

    string.h

---
+++ src/string.h
@@ -0,0 +1,19 @@
+/*
+ * string.h
+ *
+ *
+ * Copyright (C) 2017  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __STRING__
+#define __STRING__
+
+
+
+
+
+#endif
