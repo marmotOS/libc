44e482f4e1b42875c42d3f8e7a72f154c9890d6b
Bryant Moscon <bmoscon@gmail.com>
2017-02-02 18:55:15 -0500

    Complete string.h header file

--- src/string.h
+++ src/string.h
@@ -14,14 +14,29 @@
 
 #include <stddef.h>
 
-void *memchr(const void *, const int, const size_t);
-int   memcmp(const void*, const void*, const size_t);
-void *memcpy(void *, const void*, const size_t);
-void *memmove(void *, const void *, const size_t);
-void *memset(void *, const int, const size_t);
+void  *memchr(const void *, const int, const size_t);
+int    memcmp(const void*, const void*, const size_t);
+void  *memcpy(void *, const void*, const size_t);
+void  *memmove(void *, const void *, const size_t);
+void  *memset(void *, const int, const size_t);
 
-char *strcat(char*, const char*);
-char *strchr(const char*, const int);
+char  *strcat(char*, const char*);
+char  *strchr(const char*, const int);
+int    strcmp(const char*, const char*);
+int    strcoll(const char*, const char*);
+char  *strcpy(char *, const char*);
+size_t strcspn(const char*, const char*);
+char  *strerror(const int);
+size_t strlen(const char*);
+char  *strncat(char *, const char*, const size_t);
+int    strncmp(const char*, const char*, const size_t);
+char  *strncpy(char*, const char*, const size_t);
+char  *strpbrk(const char*, const char*);
+char  *strrchr(const char*, const int`);
+size_t strspn(const char*, const char*);
+char  *strstr(const char*, const char*);
+char  *strtok(char *, const char*);
+size_t strxfrm(char *, const char*, const size_t);
 
 
 #endif
