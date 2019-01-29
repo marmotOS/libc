b6f847bafae7d2a01fbe6e7a13450f2227881b66
Bryant Moscon <bmoscon@gmail.com>
2019-01-28 22:11:39 -0500

    Create signal.h

---
+++ src/signal.h
@@ -0,0 +1,18 @@
+/*
+ * signal.h
+ *
+ *
+ * Copyright (C) 2019  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Please see the LICENSE file for the terms and conditions associated with 
+ * the use of this software.
+ *
+ */
+
+#ifndef __SIGNAL__
+#define __SIGNAL__
+
+
+
+
+#endif
